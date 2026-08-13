b8f1f6553ec93b12066006544718cd58e9ddeb2a
fihmandbarmu <care4fihmchangeh@gmail.com>
2026-08-13 22:20:00 +0100

    Add button click event to change title color and text

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// First, we find our button and our title in the HTML
+const myButton = document.getElementById("amazingButton");
+const myTitle = document.getElementById("myTitle");
+
+// Next, we tell the button what to do when clicked
+myButton.addEventListener("click", function() {
+  myTitle.style.color = "red";
+  myTitle.innerHTML = "Ta-da! The amazing trick worked!";
+});
